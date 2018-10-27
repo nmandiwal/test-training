45f22bedee0ff37645b075191c6e5c2b8ba3e862
narendra mandiwal <nmandiwal@sapient.com>
2018-10-27 14:57:21 +0100

    chak lo

---
+++ sprint-rest-data/src/main/java/hello/Person.java
@@ -0,0 +1,24 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ sprint-rest-data/src/main/java/hello/PersonRepository.java
@@ -0,0 +1,8 @@
+package hello;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "persons", path = "persons")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+}
